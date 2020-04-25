fa17b712889b4d80437af56ab12792306b414229
tianss <5254610+tianssde@user.noreply.gitee.com>
2020-04-25 18:47:07 +0800

    update install.sh.

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 install_gost(){
+    install_tool
     check_git
     git clone https://gitee.com/tianssde/Gost-Utils.git
 chmod -R 777 Gost-Utils && cd Gost-Utils && bash gost-Utils.sh
@@ -20,6 +21,20 @@ check_git() {
 install_git() {
 	yum install git
     git init
+}
+
+install_tool() {
+    echo "===> Start to install tool"    
+    if [ -x "$(command -v yum)" ]; then
+        command -v curl > /dev/null || yum install -y curl
+        systemctl stop firewalld.service
+        systemctl disable firewalld.service
+    elif [ -x "$(command -v apt)" ]; then
+        command -v curl > /dev/null || apt install -y curl
+    else
+        echo "Package manager is not support this OS. Only support to use yum/apt."
+        exit -1
+    fi 
 }
 
 install_gost
